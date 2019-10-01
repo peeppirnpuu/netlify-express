d2edc84358811ebae6f813fefd46052f78e230b6
Unknown <peep@pirnpuu.ee>
2019-10-01 13:30:39 +0300

    Implement Shopify /products endpoint

--- express/server.js
+++ express/server.js
@@ -3,16 +3,50 @@ const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
-const bodyParser = require('body-parser');
-
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const request = require('request-promise');
+
+const validateSignature = (query) => {
+  var parameters = [];
+  for (var key in query) {
+    if (key != 'signature') {
+      parameters.push(key + '=' + query[key])
+    }
+  }
+  var message = parameters.sort().join('');
+  var digest = crypto.createHmac('sha256', apiSecret).update(message).digest('hex');
+  return digest === query.signature;
+};
+
+router.get('/products', (req, res) => {
+  if (req.query && Object.keys(req.query).length) {
+    const validSignature = validateSignature(req.query);
+
+    if (validSignature) {
+      const { shop } = req.query;
+
+      const shopRequestUrl = 'https://' + shop + '/admin/api/2019-07/products.json';
+      const shopRequestHeaders = {
+        'X-Shopify-Access-Token': accessToken,
+      };
+
+      request.get(shopRequestUrl, { headers: shopRequestHeaders })
+      .then((shopResponse) => {
+        console.log('shopResponse', shopResponse);
+        res.status(200).end(shopResponse);
+      })
+      .catch((error) => {
+        res.status(error.statusCode).send(error.error.error_description);
+      });
+    } else {
+      return res.status(400).send('Can not validate signature.');
+    }
+  } else {
+    return res.status(400).send('Not valid request.');
+  }
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
